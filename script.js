f8dd8c45bc83db59a2744e052a351f7629296af9
marquesarendse <marquesarendse@gmail.com>
2024-03-08 15:46:09 +0200

    First init

---
+++ script.js
@@ -0,0 +1,14 @@
+function submitForm() {
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    // Perform validation or send data to the server as needed
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Password:', password);
+  
+    // For simplicity, let's just display an alert
+    alert('Sign up successful!');
+  }
+  
